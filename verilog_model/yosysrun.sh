2cc161ab67f9fc7400fdce4889e2cc6fb39d5b69
Abdulbitm <160620896+Abdulbitm@users.noreply.github.com>
2024-03-01 19:30:37 +0530

    Update yosysrun.sh

--- verilog_model/yosysrun.sh
+++ verilog_model/yosysrun.sh
@@ -1,1 +1,16 @@
+# read design
+
+read_verilog Abdul.v
+
+# generic synthesis
+synth -top Abdul  
+
+# mapping to mycells.lib
+dfflibmap -liberty /Users/vinayrayapati/Desktop/Vinay/RISC-V/iiitb_rv32i-main/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+proc; opt
+abc -liberty /Users/vinayrayapati/Desktop/Vinay/RISC-V/iiitb_rv32i-main/lib/sky130_fd_sc_hd__tt_025C_1v80.lib -script +strash;scorr;ifraig;retime,{D};strash;dch,-f;map,-M,1,{D}
+clean
+flatten
+# write synthesized design
+write_verilog -noattr iiitb_rv32i_synth.v
 
