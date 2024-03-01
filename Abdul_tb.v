0c943216eb0a8d476fa0102f4559e412281d69d5
Abdulbitm <160620896+Abdulbitm@users.noreply.github.com>
2024-03-01 19:16:19 +0530

    Update Abdul_tb.v

--- Abdul_tb.v
+++ Abdul_tb.v
@@ -3,7 +3,7 @@ module Abdul_tb;
 reg clk,RN;
 wire [31:0]WB_OUT,NPC;
 
-Abdul rv32(clk,RN,NPC,WB_OUT);
+Abdul Abdul32(clk,RN,NPC,WB_OUT);
 
 
 always #3 clk=!clk;
@@ -12,8 +12,8 @@ initial begin
 RN  = 1'b1;
 clk = 1'b1;
 
-$dumpfile ("iiitb_rv32i.vcd"); //by default vcd
-$dumpvars (0, iiitb_rv32i_tb);
+  $dumpfile ("Abdul.vcd"); //by default vcd
+  $dumpvars (0, Abdul_tb);
   
   #5 RN = 1'b0;
   
