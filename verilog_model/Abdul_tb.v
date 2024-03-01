cedd772aa617fea30a714d78753a28c86459a0f5
Abdulbitm <160620896+Abdulbitm@users.noreply.github.com>
2024-03-01 19:29:52 +0530

    Update Abdul_tb.v

--- verilog_model/Abdul_tb.v
+++ verilog_model/Abdul_tb.v
@@ -1,1 +1,24 @@
+module Abdul_tb;
+
+reg clk,RN;
+wire [31:0]WB_OUT,NPC;
+
+Abdul Abdul32(clk,RN,NPC,WB_OUT);
+
+
+always #3 clk=!clk;
+
+initial begin 
+RN  = 1'b1;
+clk = 1'b1;
+
+  $dumpfile ("Abdul.vcd"); //by default vcd
+  $dumpvars (0, Abdul_tb);
+  
+  #5 RN = 1'b0;
+  
+  #300 $finish;
+
+end
+endmodule
 
