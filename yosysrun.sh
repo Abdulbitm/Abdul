3a3297b491113c88325d31d60c0c34eb3e4366eb
Abdulbitm <160620896+Abdulbitm@users.noreply.github.com>
2024-03-02 07:24:58 +0530

    Update yosysrun.sh

--- yosysrun.sh
+++ yosysrun.sh
@@ -6,9 +6,9 @@ read_verilog Abdul.v
 synth -top hello  
 
 # mapping to mycells.lib
-dfflibmap -liberty /home/vsduser/Desktop/hello/RISC-V/hello-main/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+dfflibmap -liberty /home/vsduser/Documents/hello/RISC-V/hello-main/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 proc; opt
-abc -liberty /home/vsduser/Desktop/hello/RISC-V/hello-main/lib/sky130_fd_sc_hd__tt_025C_1v80.lib -script +strash;scorr;ifraig;retime,{D};strash;dch,-f;map,-M,1,{D}
+abc -liberty /home/vsduser/Documents/hello/RISC-V/hello-main/lib/sky130_fd_sc_hd__tt_025C_1v80.lib -script +strash;scorr;ifraig;retime,{D};strash;dch,-f;map,-M,1,{D}
 clean
 flatten
 # write synthesized design
